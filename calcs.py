05578180f1af9db78ff8daa1f79f868d097a9a2b
Craig McIndoe <craig.mcindoe@gmail.com>
2017-07-06 23:26:13 +0100

    Implemented SMA calculation.

---
+++ calcs.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+
+class Calcs:
+
+    def __init__(self):
+        pass
+
+    # Calculate SMA from OHLC data
+    def calc_sma(self, data, ma):
+        result = {}
+        count = 0
+        ma_prices = deque()
+
+        for item in data:
+            count += 1
+            if count < ma:
+                ma_prices.append(item[4])
+                result[item[0]] = 0.0
+            elif count == ma:
+                ma_prices.append(item[4])
+                cum_total = 0.0
+
+                for val in ma_prices:
+                    cum_total += val
+
+                result[item[0]] = cum_total / ma
+            else:
+                ma_prices.popleft()
+                ma_prices.append(item[4])
+                cum_total = 0.0
+
+                for val in ma_prices:
+                    cum_total += val
+
+                result[item[0]] = cum_total / ma
+
+            print('Price: ' + str(item[4]) + ' ' + '\tCounter: ' +
+                  str(count) + '\tSMA: ' + str(result[item[0]]))
+            print(str(ma_prices))
+        print(str(result))
+
+        return result
--- data.py
+++ data.py
@@ -2,6 +2,7 @@ import krakenex
 import csv
 import os
 import json
+import calcs
 
 
 class Data(object):
@@ -17,32 +18,33 @@ class Data(object):
         self.ohlc_data = []
         self.ohlc_last_file = datadir + '\\' + tradingpair + str(timeframe) + '_OHLC_LASTID.json'
         self.ohlc_last_id = 0
-        # self.crypto_bal = 0.0
-        # self.fiat_bal = 0.0
+        self.calcs_data = {}
         return
 
     def _parse_ohlc(self, result):
         print('Parsing OHLC data.')
+        # Parse the strings to floats
         for item in result:
-            if item[0] > self.ohlc_last_id:
-                result.remove(item)
-            else:
-                item[1] = float(item[1])
-                item[2] = float(item[2])
-                item[3] = float(item[3])
-                item[4] = float(item[4])
-                item[5] = float(item[5])
-                item[6] = float(item[6])
+            item[1] = float(item[1])
+            item[2] = float(item[2])
+            item[3] = float(item[3])
+            item[4] = float(item[4])
+            item[5] = float(item[5])
+            item[6] = float(item[6])
+        # Remove last item (since this is always the un-committed frame
+        del result[-1]
         return result
 
     def import_ohlc(self):
-
         if os.path.isfile(self.ohlc_file) and os.path.isfile(self.ohlc_last_file):
             print('Importing existing OHLC data.')
+
             with open(self.ohlc_file, 'r', newline='') as f:
                 self.ohlc_data = json.load(f)
             with open(self.ohlc_last_file, 'r', newline='') as f:
                 self.ohlc_last_id = json.load(f)
+
+            self._update_calcs_keys()
         else:
             print('Existing OHLC data not found.')
             self.refresh_ohlc()
@@ -61,11 +63,24 @@ class Data(object):
 
         for item in ohlc_p:
             self.ohlc_data.append(item)
+
+        self._update_calcs_keys()
+        return
+
+    def _update_calcs_keys(self):
+        for item in self.ohlc_data:
+            if item[0] not in self.calcs_data:
+                self.calcs_data[item[0]] = {}
+        return
+
+    def update_sma(self, ma):
+        c = calcs.Calcs()
+        c.calc_sma(self.ohlc_data, ma)
         return
 
     def export_ohlc(self):
-
         print('Exporting OHLC / Last ID data.')
+
         with open(self.ohlc_file, 'w', newline='') as f:
             json.dump(self.ohlc_data, f)
         with open(self.ohlc_last_file, 'w', newline='') as f:
@@ -73,8 +88,8 @@ class Data(object):
         return
 
     def export_ohlc_csv(self):
-
         print('Exporting OHLC data to CSV.')
+
         with open(self.ohlc_file_csv, 'w', newline='') as f:
             w = csv.writer(f)
             w.writerows(self.ohlc_data)
